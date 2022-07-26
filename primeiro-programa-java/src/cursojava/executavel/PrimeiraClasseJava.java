ef8573ac2b658c69ac05660385a8aa654c73f2e3
darley <darleychagas03@gmail.com>
2022-07-25 21:17:26 -0400

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java -do curso formação java web");
+		System.out.println("Java é uma linguagem muito almejada por empresas de grande porte");
+	}
+
+}
