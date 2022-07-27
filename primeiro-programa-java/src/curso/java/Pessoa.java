59f7c4c418b59b9ec1664284dd2056ed2167c239
darley <darleychagas03@gmail.com>
2022-07-26 22:19:01 -0400

    alteração

--- primeiro-programa-java/src/curso/java/Pessoa.java
+++ primeiro-programa-java/src/curso/java/Pessoa.java
@@ -1,5 +1,17 @@
 package curso.java;
 
 public class Pessoa {
+	
+	private String nome;
+	
+	private String sobrenome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
